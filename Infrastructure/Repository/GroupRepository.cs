10d6fb57454227e8a2cafa323e202761b5a438fc
p1sh4k <39710577+p1sh4k@users.noreply.github.com>
2018-05-28 23:37:32 +0300

    Add files via upload

---
+++ Infrastructure/Repository/GroupRepository.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Infrastructure.Config;
+
+namespace Infrastructure.Repository
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public GroupRepository(IOptions<Settings> settings)
+        {
+            _context = new DatabaseContext(settings);
+        }
+
+        public async Task<IEnumerable<GroupModel>> GetAllGroups()
+        {
+            return await _context.Groups.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<GroupModel> GetGroup(string id)
+        {
+            var filter = Builders<GroupModel>.Filter.Eq("_Id", ObjectId.Parse(id));
+            return await _context.Groups
+                .Find(filter)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task AddGroup(GroupModel item)
+        {
+            await _context.Groups.InsertOneAsync(item);
+        }
+
+        public async Task<DeleteResult> RemoveGroup(string id)
+        {
+            return await _context.Groups.DeleteOneAsync(
+                Builders<GroupModel>.Filter.Eq("_Id", ObjectId.Parse(id)));
+        }
+
+        public async Task<UpdateResult> UpdateGroup(string id, string name)
+        {
+            var filter = Builders<GroupModel>.Filter.Eq("_Id", ObjectId.Parse(id));
+            var update = Builders<GroupModel>.Update
+                .Set(s => s.Name, name)
+                .CurrentDate(s => s.ModificationDate);
+
+            return await _context.Groups.UpdateOneAsync(filter, update);
+        }
+    }
+}
---
+++ Infrastructure/Repository/RetailRepository.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Infrastructure.Config;
+
+namespace Infrastructure.Repository
+{
+    public class RetailRepository : IRetailRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public RetailRepository(IOptions<Settings> settings)
+        {
+            _context = new DatabaseContext(settings);
+        }
+
+        public async Task<IEnumerable<RetailModel>> GetAllRetails()
+        {
+            return await _context.Retails.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<RetailModel> GetRetail(string id)
+        {
+            var filter = Builders<RetailModel>.Filter.Eq("_Id", ObjectId.Parse(id));
+            return await _context.Retails
+                .Find(filter)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task AddRetail(RetailModel item)
+        {
+            await _context.Retails.InsertOneAsync(item);
+        }
+
+        public async Task<DeleteResult> RemoveRetail(string id)
+        {
+            return await _context.Retails.DeleteOneAsync(
+                Builders<RetailModel>.Filter.Eq("_Id", ObjectId.Parse(id)));
+        }
+
+        public async Task<UpdateResult> UpdateRetail(string id, string name)
+        {
+            var filter = Builders<RetailModel>.Filter.Eq("_Id", ObjectId.Parse(id));
+            var update = Builders<RetailModel>.Update
+                .Set(s => s.Name, name)
+                .CurrentDate(s => s.ModificationDate);
+
+            return await _context.Retails.UpdateOneAsync(filter, update);
+        }
+    }
+}
